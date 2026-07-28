52445ce15dd027711b7a850632304c7f456fd915
Anthony Popoca <tonystr97@gmail.com>
2026-07-27 22:08:56 -0500

    exercises for day2

---
+++ day2/variables.py
@@ -0,0 +1,11 @@
+#Day 2: 30 Days of python programming
+firstName = 'This' 
+lastName = 'Nanji'
+fullName = firstName + ' ' + lastName
+country = "States"
+city = 'Worth'
+age = 28
+year = 26
+is_married = False
+is_true = True
+is_light_on = True
