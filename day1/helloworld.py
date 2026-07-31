6c4de9a18d44532f544875f5c431b440f9f147f9
Popoca <tonystr97@gmail.com>
2026-07-31 00:10:43 -0500

    saved changes

---
+++ day1/helloworld.py
@@ -0,0 +1,30 @@
+import math
+#1 simple math
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+#2 strings
+print("Nanji")
+print("Tempest")
+print("Jira")
+print("I am attempting the 30 days of python")
+
+#3 check data types
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Nanji"))
+print(type("Tempest"))
+print(type("Jira"))
+
+#4 Euclidean distance
+
+print(math.sqrt((2-10)**2 + (10-8)**2))
--- day3/exercise.py
+++ day3/exercise.py
@@ -5,16 +5,26 @@ complex_num = 5-5j
 
 #2 script trianlge
 base = float(input("Enter base: " ))
-height = input("Enter height: " )
-area = 0.5 * float(base) * float(height)
+height = float(input("Enter height: " ))
+area = 0.5 * base * height
 print(f'The area of th triangle is {area}')
 
 #3 script perimeter
-side_a = input("Enter side a: ")
-side_b = input("Enter side b: ")
-side_c = input("Enter side c: ")
-print(f"The perimeter of the triangle is {int(side_a) + int(side_b) + int(side_c)}")
+side_a = int(input("Enter side a: "))
+side_b = int(input("Enter side b: "))
+side_c = int(input("Enter side c: "))
+print(f"The perimeter of the triangle is {side_a + side_b + side_c}")
 
 #4 rectangle area
-length = input("Enter length: ")
-length = input("Enter width: ")
+length = int(input("Enter length: "))
+width = int(input("Enter width: "))
+rect_area = length * width
+rect_perimeter = 2 * (length +width)
+print(f"The area is {rect_area} and the perimeter is {rect_perimeter}")
+
+#5 circle
+circ_radius = float(input("Enter a radius: "))
+circ_area = 3.14 * (circ_radius **2)
+circ_circum = 2 * 3.14 * circ_radius
+
+
