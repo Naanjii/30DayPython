55530aaf367d216181028f133e5b7a959685d294
Popoca <tonystr97@gmail.com>
2026-07-31 00:09:29 -0500

    Create exercise.py
    started day 3 exercises

---
+++ day3/exercise.py
@@ -0,0 +1,20 @@
+#1 declare age and height
+age = 29
+height = 5.10
+complex_num = 5-5j
+
+#2 script trianlge
+base = float(input("Enter base: " ))
+height = input("Enter height: " )
+area = 0.5 * float(base) * float(height)
+print(f'The area of th triangle is {area}')
+
+#3 script perimeter
+side_a = input("Enter side a: ")
+side_b = input("Enter side b: ")
+side_c = input("Enter side c: ")
+print(f"The perimeter of the triangle is {int(side_a) + int(side_b) + int(side_c)}")
+
+#4 rectangle area
+length = input("Enter length: ")
+length = input("Enter width: ")
