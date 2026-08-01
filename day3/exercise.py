5bf8a28aca160a1ce1460b9e9611e5a16bd03c48
Popoca <tonystr97@gmail.com>
2026-08-01 00:54:24 -0500

    continued day 3 exercises.

--- day3/exercise.py
+++ day3/exercise.py
@@ -1,3 +1,5 @@
+import math
+
 #1 declare age and height
 age = 29
 height = 5.10
@@ -27,4 +29,43 @@ circ_radius = float(input("Enter a radius: "))
 circ_area = 3.14 * (circ_radius **2)
 circ_circum = 2 * 3.14 * circ_radius
 
+#6 slopes
+slope_1 = 2
+slope_2 = math.sqrt(((2-6)**2) + ((2-10)**2))
+print(slope_1 <= slope_2)
 
+#7 length
+drag_leng = len("dragon")
+python_leng = len("python")
+#falsy compare
+print(python_leng != drag_leng)
+#if on
+print("on" in "dragon" and "on" in "python")
+
+#jargon
+print("jargon" in  "I hope this course is not full of jargon")
+
+#not
+print(not "on" in "dragon" and "python")
+
+python_float = float(python_leng)
+python_string = str(python_leng)
+
+#floor and int
+print(7//3 == int(2.7))
+
+#check types
+print (type('10') == type(10))
+
+#check if equal
+print(int(9.8) == -10)
+
+#script hourly
+hrs = int(input("Enter hours: "))
+rate = int(input("Enter rate per hour: "))
+print(f'Your weekly earning is {hrs * rate}')
+
+#sec to live
+years_lived = int(input('Enter the number of years you have lived: '))
+years_to_sec = years_lived * 31536000
+print(f'You have lived for {years_to_sec} seconds.')
