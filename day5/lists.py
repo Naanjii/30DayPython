4f731b1096c4ae4ee447a1a70b44a193a30aff54
Popoca <tonystr97@gmail.com>
2026-08-03 23:19:32 -0500

    began day5 exercises and got up to exercise 12

---
+++ day5/lists.py
@@ -0,0 +1,26 @@
+
+#exercise1-4
+nolist = []
+items = ['controller','console','charger','disc','monitor']
+print(items)
+print(len(items))
+first_item = items[0]
+print(first_item)
+middle_item = items[2]
+print(middle_item)
+last_item = items[4]
+print(last_item)
+
+#exercise 5
+mixed_data = ['Nanji', 28, 68, 'Dating', 'Apartment']
+
+#exercise 6
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+print(it_companies)
+print(it_companies[0])
+print(it_companies[3])
+print(it_companies[6])
+it_companies[3] = 'Samsung'
+print(it_companies)
+it_companies.append("ROGSTRX")
+print(it_companies)
